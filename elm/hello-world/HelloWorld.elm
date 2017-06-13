ac9abfe98841ff7c25db471c6191fe35fdcf50b2
Kaitlyn Tierney <k@krtierney.com>
2017-06-13 16:03:50 +0100

    elm it up

---
+++ elm/hello-world/HelloWorld.elm
@@ -0,0 +1,21 @@
+{-
+   This is a "stub" file.  It's a little start on your solution. It's not a
+   complete solution though; you have to write some code.
+
+   The module name is expected by the test program and must match the name of this
+   file. It has to stay just the way it is.
+-}
+
+
+module HelloWorld exposing (..)
+
+-- It's good style to include any types at the top level of your modules.
+
+
+helloWorld : Maybe String -> String
+helloWorld name =
+    case name of
+      Nothing ->
+        "Hello, World!"
+      Just name ->
+        "Hello, " ++ name ++ "!"
