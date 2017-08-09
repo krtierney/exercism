06e5e758588ef4fbded6359b5ac5bd1840d4a740
Kaitlyn Tierney <k@krtierney.com>
2017-08-09 17:00:52 +0100

    new exercises fetched

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,14 @@
 module Bob exposing (..)
+
+
+hey : String ->String
+hey say = 
+  last_char=
+    right 1 say
+  if last_char == '?' then
+    "Sure"
+  else if last_char == '!' then
+   "Whoa, chill out!"
+  else
+    "Whatever."
+    
